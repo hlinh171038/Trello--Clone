4ac314a7e1f78f1e8218eb323fca79fca4fff226
HL <hoanglinh171038@gmail.com>
2023-11-24 14:51:04 +0700

    first create server

--- README.md
+++ README.md
@@ -4,11 +4,10 @@ library:
 - Authentication
  + npm install @clerk/nextjs
  + prisma + planetscale(free and unlimited  data)
+ + npx prisma studio
+ + npm i zod
 
-? setup prisma to use server action
- 1. set up planetscale --> take link data base + prisma.schema
- 2. change link database in .env file
- 3. ddefined first modal 
- 4. npx prisma generate
- 5. npx prisma db push
+? try to create server side with prisma studio
+1. server side create in (actions/create-board)
+2. form --> action ={create}
 
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,23 @@
 
+import { create } from "@/actions/create-board"
+import { Button } from "@/components/ui/button"
+
 
 const OrganizationIdPage = () =>{
+   
     return (
         <div>
-            <form>
-                <input/>
+            <form action={create}>
+                <input
+                    id="title"
+                    name="title"
+                    required
+                    placeholder="Enter a board title"
+                    className="border-black border p-1" 
+                />
+                <Button type="submit" >
+                    Submit
+                </Button>
             </form>
         </div>
     )
