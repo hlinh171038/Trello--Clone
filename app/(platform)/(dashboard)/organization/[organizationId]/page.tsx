b5ad7755fcdfb2cba675fdaf55e1ce1e343172a3
HL <hoanglinh171038@gmail.com>
2023-12-01 08:52:32 +0700

    12. infor organization + skeleton

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -3,22 +3,15 @@
 import { Button } from "@/components/ui/button"
 import { db } from "@/lib/db"
 
-import { Form } from "./form";
-import { Board } from "./board";
+
+import { Info } from "./_components/info";
 
 
 const OrganizationIdPage = async () =>{
     const boards = await db.board.findMany();
     return (
-        <div>
-            <div className="flex flex-col space-y-4">
-            <Form />
-            <div className="space-y-2">
-                {boards.map((board) => (
-                <Board key={board.id} title={board.title} id={board.id} />
-                ))}
-            </div>
-            </div>
+        <div className="w-full mb-20">
+           <Info />
         </div>
     )
 }
