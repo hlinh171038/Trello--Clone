0429c2f3ce302816caa5ca84a51a20a69feacbcb
HL <hoanglinh171038@gmail.com>
2023-12-06 15:18:15 +0700

    25.delete board(interface)

--- app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { Board } from "@prisma/client";
 import { BoardTitleForm } from "./board-title-form";
+import { BoardOpotionsProps } from "./board-options";
 
 interface BoardNavProps {
     data: Board
@@ -11,6 +12,9 @@ export const  BoardNavbar = async ({data}:BoardNavProps) =>{
     return (
         <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
            <BoardTitleForm data={data} />
+           <div className="ml-auto">
+                <BoardOpotionsProps id={data.id} />
+           </div>
         </div>
     )
 }
